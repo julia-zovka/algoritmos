d72a50cf29150401b327a5962788ea8632561e0b
jzs <jzs@g2c31.cin.ufpe.br>
2025-07-15 09:56:00 -0300

    primm

--- lista4/prim.cpp
+++ lista4/prim.cpp
@@ -34,57 +34,57 @@ void prim(vector<vector<pair<int,int>>> &grafo, int s, vector<int> &distance, ve
 }
 
 
-void setEdgee(vector<vector<pair<int,int>>> &grafo, int i, int j, int w, int numEdge){
-            if(w==0){return;}
-            grafo[i].push_back({j,w});
-            grafo[j].push_back({i,w});
-            numEdge++;
-        }
-bool conexo(vector<int> &distance){
-    for(int dist:distance){
-        if(dist==INT_MAX){
-            return false;
-        }
+void setEdgee(vector<vector<pair<int,int>>> &grafo, int i, int j, int w, int &numEdge){
+    if(w==0){return;}
+    grafo[i].push_back({j,w});
+    grafo[j].push_back({i,w});
+    numEdge++;
+}
+
+
+void cam(bool &caminho, vector<int> &distance){
+    for(int tam:distance){
+        if(tam==INT_MAX){
+            caminho=false;
+            return;}
     }
-    return true;
 }
 
 int main(){
     int n,e,s,d,a,b,w;
     cin>> n >> e;
-    int numEdge;
 
     vector<vector<pair<int,int>>> grafo;
     vector<int> pred;
     vector<int> mark;
     vector<int> distance;
-    while(n!=0 && e!=0){
+    
+    while(n!=0 || e!=0){
+        vector<vector<pair<int,int>>> grafo(n, vector<pair<int,int>>());
+
+        int numEdge=0;
         pred.assign(n,-1);
         mark.assign(n,0);
         distance.assign(n,INT_MAX);
 
-
-        grafo.resize(n);
         for(int i=0;i<e;i++){
             cin>> a >> b >>w;
             setEdgee(grafo,a,b,w, numEdge);
         }
-        cin>>s>>d;
         
-        prim(grafo, s, distance,mark, pred, n);
+        prim(grafo, 0, distance,mark, pred, n);
         
+        bool caminho=true;
+        cam(caminho, distance);//ver se é conexo ou nao 
 
-        int meio=pred[d];
-        int maior=INT_MIN;
-        while(meio!=s && meio!= -1){// sem predecessor
-            if(distance[meio]>maior){
-                maior=distance[meio];}
-            meio=pred[meio];
+        if(caminho==false){cout<<"IMPOSSIBLE"<<endl;}
+        else{
+            int maior=INT_MIN;
+            for(int tam :distance){
+                if(tam>maior){maior=tam;}
+            }
+           cout<<maior<<endl;
         }
-        bool caminho=false;
-        if(meio==s){caminho=true;}
-        if(caminho){cout<<maior<<endl;}
-        if(caminho==false || numEdge<(n*(n-1))){cout<<"IMPOSSIBLE"<<endl;}
         cin>>n>>e;
     }
 }
