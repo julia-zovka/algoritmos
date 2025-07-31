6b41196a4de230b96b35e6911b00b82214af2f02
julia-zovka <jzs@cin.ufpe.br>
2025-07-30 23:49:44 -0300

    q1

--- lista5/q2.cpp
+++ lista5/q2.cpp
@@ -1,55 +1,70 @@
-/*The first line of a test case contains three integers n, m and j.
- n (2 ≤ n ≤ 40) the number of rows, m (2 ≤ n ≤ 40) the width of each row and j (0 ≤ j ≤ 20)
-  the number of times Jarmtin can avoid spikes. Then n lines containing m characters; 
-  The character 'x' will be used for the place of the treasure, '@' for an entrance (which is also an exit), '#' for walls, '.' for a safe walking tile and 's' for spikes. Note that you cannot walk into walls and the maze is completely surrounded by walls outside what you can see. 
-\There is always at least one entrance/exit and always an x where the treasure is.*/
-
 #include <vector>
 #include <iostream>
-
 using namespace std;
 
+//vou ter que considerar o caso de que o tesou pode ta pra cima pra baiso e para os dois lados da entradaaaa
+//considerar casos de looppp
 
 
+bool mazee(int i, int j, int lives, vector<vector<char>> &maze, int wid,int hght, int spikes, vector<vector<bool>> &visited){
 
-
-
-bool mazee(int i, int j, int lives, vector<vector<char>> &maze, int wid,int hght, int spikes ){
-    if (maze[i][j]=='x' && lives>=spikes/2) return true;/// chegu onde queriaa sem morrer e consegue voltar
-
-    if(j==wid){//fim da linha entao desce pra proxima
-        return mazee(i+1,0,lives,maze,wid,hght,spikes);
+    if(j>=wid || j<0 || i<0|| i>=hght){// ja foi pra pa fora da maze
+        return false;
     }
-    
+    if (maze[i][j]=='x' && lives>=spikes*2 ){ return true;}/// chegu onde queriaa sem morrer e consegue voltar
 
-    if(maze[i][j]!='#'){// se não é parede pode testar
+    if(maze[i][j]=='#'){return false;}// nao posso naquele 
 
-        if(maze[i][j]=='s' && lives!=0){//tem spike mas ainda tem vida,  vai por ali
-            lives--;
-            mazee(i,j+1, lives, maze, wid, hght,spikes);
-        }
-        if(maze[i][j]=='.'){
-            mazee(i,j+1, lives, maze, wid, hght,spikes);
+    else{// se não é parede pode testar// se náo [arede  . =andar ou s=menos vida ou x=cehoug
+        if(visited[i][j]==true){return false;}//loop
+
+        else{//quando ainda t[a ] false==primeira vez que passou pelo lugar na tentativa de ida
+            visited[i][j]=true;
         }
 
+        if(maze[i][j]=='s'){//tem spike mas pde tentar  vai por ali
+            if(lives==0){return false;}//moreuuu
+            else{
+                spikes++;}
+        }
+        //tenta todas as direc;oes
+        if(mazee(i-1,j, lives, maze, wid, hght,spikes,visited)){return true;}
+        if(mazee(i+1,j, lives, maze, wid, hght,spikes,visited)){return true;}
+        if(mazee(i,j-1, lives, maze, wid, hght,spikes,visited)){return true;}
+        if(mazee(i,j+1, lives, maze, wid, hght,spikes,visited)){return true;}
+        //nenhuma resultou em caminho
+        visited[i][j]=false;
+        return false;
+        
     }
-
 }
+
 int main(){
-    int rows,w,lives, spikes;
+    int rows,w,lives, spikes, co_i,co_j;
     char c;
+    spikes=0;
     cin>>rows>>w>>lives;
     vector<vector<char>> maze(rows);//// ###    ...   @@@   SSS  XXX
+    vector<vector<bool>> visited(rows,vector<bool> (w,false));
 
     //popular cada linha da maze
     for(int i=0;i<rows;i++){
-        cin>>c;
-        if (c=='s'){
-            spikes++;
+        for(int y=0;y<w;y++){
+            cin>>c;
+            if (c=='s'){
+            }
+            if(c=='@'){
+                co_i=i;
+                co_j=y;
+            }
+            maze[i].push_back(c);
         }
-        maze[i].push_back(c);
     }
-
-    //comecar a passear pela maze // come;ar backtracking
+    if(mazee(co_i,co_j,lives,maze, w, rows, spikes,visited)){
+        cout<<"SUCCESS"<<endl;
+    }
+    else{
+        cout<<"IMPOSSIBLE"<<endl;
+    }
 
 }
